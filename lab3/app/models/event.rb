9261e801c7106b7ec2c3ca2c79d7ba6210e63ee8
rjmarin <rjmarin@miuandes.cl>
2019-03-27 11:31:24 -0300

    add some method

--- lab3/app/models/event.rb
+++ lab3/app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+
+
+  def last_attendance()
+    place =Event.select(:id).where(id: self.id).first
+    event = EventInformation.where(event_id: place).order(:date).last
+    c = TicketType.where(event_information_id: event.id).count
+    return c
+
+  end
 end
--- lab3/app/models/event_information.rb
+++ lab3/app/models/event_information.rb
@@ -1,2 +1,19 @@
 class EventInformation < ApplicationRecord
+
+
+  def most_ticket_sold()
+    most = EventInformation.select('event:information.*, count(ticket_order_id) as count')
+               .group(:ticket_order_id).order(count).last
+    return most
+
+  end
+
+  def highest_revenue()
+      event = TicketType.select('ticket_type.* , SUM(price) as sum').group(:event_id).order(sum).last
+      return event
+
+
+  end
+
 end
+
