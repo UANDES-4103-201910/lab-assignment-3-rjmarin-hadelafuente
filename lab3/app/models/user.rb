86a29ed38a3fcdf14726cd6efb44510af39cd4c0
hdelafuente <hadelafuente@miuandes.cl>
2019-03-28 16:10:05 -0300

    master: * User methods almost done, need testing

--- lab3/app/models/user.rb
+++ lab3/app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
   def most_expensive_ticket_bought
-    tickets = TicketRelation.where(TicketRelation.user.id = this.id)
-    for ticket in tickets
-      puts ticket
-    end
+    orders = Order.select('amount_payed').where(user_id=this.id)
+    amounts = []
+    orders.each {|order|
+      amounts.append(order.amount_payed)
+    }
+    return amounts.max
+  end
 
+  def most_expensive_ticket_bought_between(date_1, date_2)
+    orders = Order.select('amount_payed').where(Date.parse(date_1) < Order.created_at and Date.parse(date_2) > Order.created_at)
+    amounts = []
+    orders.each {|order|
+      amounts.append(order.amount_payed)
+    }
+    return amounts.max
   end
 end
