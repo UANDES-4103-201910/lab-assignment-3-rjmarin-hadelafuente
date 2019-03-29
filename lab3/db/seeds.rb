8dfffe0ab601ed1a754bd54298e74e198c6bcca9
rjmarin <rjmarin@miuandes.cl>
2019-03-28 22:11:05 -0300

    *fixed seed

--- lab3/db/seeds.rb
+++ lab3/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([
+users = User.new([
                   {name: "Raimundo", last_name: "Marin", email:"rjmarin@miuandes.cl", phone:12345678, password:"12345", address:"Algun lugar en Bassel"},
                   {name: "Hugo", last_name: "de la Fuente  ", email:"hadelafuente@miuandes.cl", phone:12345678, password:"123455", address:"Algun lugar en La Gloria"},
                   {name: "Lucho", last_name: "Figueroa", email:"asdasd@miuandes.cl", phone:12345678, password:"123455", address:"Algun lugar en Las Condes"},
@@ -14,19 +14,19 @@ users = User.create([
                   {name: "Alf", last_name: "Zuniga", email:"asdfg@miuandes.cl", phone:12345678, password:"123455", address:"Algun lugar en camino la Fuente"}])
 users.save
 
-event_1 = Venue.create()
+event_1 = Venue.new()
 event_1.name="PING"
 event_1.address="Cerro Kenny"
 event_1.capacity=300
 event_1.save
 
-event_2 = Venue.create()
+event_2 = Venue.new()
 event_2.name="Gala"
 event_2.address="Club Hipico"
 event_2.capacity=3000
 event_2.save
 
-event_3 = Venue.create()
+event_3 = Venue.new()
 event_3.name="Mea Culpa"
 event_3.address="Espacio Riesco"
 event_3.capacity=10000
